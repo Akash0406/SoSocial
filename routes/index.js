e3d159a594a590f2e81c5ef6f3fc07007f22ed42
Akash0406 <startx0406@gmail.com>
2022-01-12 20:28:49 +0530

    Added User controller & Routes and installed ejs

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const homeController = require('../controllers/home_controller')
+    // const userController = require
 
 router.get('/', homeController.home);
-
+router.use('/users', require('./users'))
 
 
 module.exports = router;
