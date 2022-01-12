d752fc56c98ac03f6a7023b1c0213865b9d8310a
Akash0406 <startx0406@gmail.com>
2022-01-13 00:17:49 +0530

    Setup Mongoose and Schema Model

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
 
 app.use(express.static('./assets'))
 
@@ -14,6 +15,7 @@ app.set('layout extractScripts', true);
 // use express router
 app.use('/', require('./routes'));
 
+// setup the view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
