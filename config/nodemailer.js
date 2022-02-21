585774039062b832c4b0780732c03f838a1aada5
Akash0406 <starx0406@gmail.com>
2022-02-21 21:24:47 +0530

    Mailer congig completed

---
+++ config/nodemailer.js
@@ -0,0 +1,34 @@
+const nodemailer = require('nodemailer');
+const ejs = require('ejs');
+const path = require('path');
+
+
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+        user: 'starx.897@gmail.com',
+        pass: 'Destination0406'
+    }
+});
+
+let renderTemplate = (data, relativePath) => {
+    let mailHTML;
+    eje.renderFile(
+        path.join(__dirname, '../views/mailers', relativePath),
+        data,
+        function (err, template) {
+            if (err) { console.log('Error in rendering template'); return; }
+
+            mailHTML = template;
+        }
+    )
+    return mailHTML;
+}
+
+module.exports = {
+    transporter: transporter,
+    renderTemplate = renderTemplate
+}
